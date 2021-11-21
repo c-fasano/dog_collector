ac623bbe315058d56c3e4bd778601a4f477416be
Cole-Fasano <cole.fasano@outlook.com>
2021-11-21 14:49:45 -0600

    successful migration

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Dog
 
 # Create your views here.
 
@@ -8,21 +9,8 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-class Dog:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-dogs = [
-  Dog('Lolo', 'tabby', 'Kinda rude.', 3),
-  Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-  Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
-  Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
 def dogs_index(request):
+  dogs = Dog.objects.all()
   return render(request, 'dogs/index.html', { 'dogs': dogs })
 
 def home(request):
