919e574ad175f83274f43e68728ff02f21db6c79
Cole-Fasano <cole.fasano@outlook.com>
2021-11-21 11:32:08 -0600

    create index view

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,20 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+class Dog:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Lolo', 'tabby', 'Kinda rude.', 3),
+  Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+  Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
+  Dog('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+def dogs_index(request):
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
