e744c5340e2827e2bee800381b6091b3f282828f
Cole-Fasano <cole.fasano@outlook.com>
2021-11-28 13:16:00 -0600

    write add walk view function:

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 from .forms import WalkForm
@@ -18,6 +18,14 @@ def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
   walk_form = WalkForm()
   return render(request, 'dogs/detail.html', { 'dog': dog, 'walk_form': walk_form })
+
+def add_walk(request, dog_id):
+  form = WalkForm(request.POST)
+  if form.is_valid():
+    new_walk = form.save(commit=False)
+    new_walk.dog_id = dog_id
+    new_walk.save()
+  return redirect('dogs_detail', dog_id=dog_id)
 
 class DogCreate(CreateView):
   model = Dog
