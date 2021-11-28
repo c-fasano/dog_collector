09f8ed4b42bfee324b4083487613907d82205334
Cole-Fasano <cole.fasano@outlook.com>
2021-11-28 12:11:31 -0600

    Create walk model

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,13 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
+TIMES = (
+  ('M', 'Morning'),
+  ('A', 'Afternoon'), 
+  ('E', 'Evening'), 
+  ('N', 'Night'),
+)
+
 class Dog(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
@@ -15,3 +22,15 @@ class Dog(models.Model):
   def get_absolute_url(self):
       return reverse("dogs_detail", kwargs={"dog_id": self.id})
   
+class Walk(models.Model):
+  date = models.DateField()
+  time = models.CharField(
+    max_length=1,
+    choices=TIMES,
+    default=TIMES[0][0]
+  )
+
+  dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_walk_display()} on {self.date}"
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('dogs/', views.dogs_index, name='dogs_index'),
-  path('dogs<int:dog_id>/', views.dogs_detail, name='dogs_detail'),
+  path('dogs/<int:dog_id>/', views.dogs_detail, name='dogs_detail'),
   path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
-  path('dogs/<int:pk>/delete/', views.DogUpdate.as_view(), name='dogs_update'),
-  path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete')
+  path('dogs/<int:pk>/update', views.DogUpdate.as_view(), name='dogs_update'),
+  path('dogs/<int:pk>/delete', views.DogDelete.as_view(), name='dogs_delete')
 ]
